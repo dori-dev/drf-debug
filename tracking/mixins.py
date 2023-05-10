a3cb916e57f710319936a52b443174409ee552fc
dori-dev <mr.dori.dev@gmail.com>
2023-05-10 20:09:55 +0330

    save logs in database

--- tracking/mixins.py
+++ tracking/mixins.py
@@ -1,5 +1,9 @@
 from tracking.base_mixins import BaseLoggingMixin
 
+from tracking.models import APIRequestLog
+
 
 class LoggingMixin(BaseLoggingMixin):
-    pass
+    def handle_log(self, request, response):
+        if self.should_log(request, response):
+            APIRequestLog(**self.log).save()
